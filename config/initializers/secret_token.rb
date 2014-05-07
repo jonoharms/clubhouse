80681f1bef0b53c1463ecb60860f0493d6b811e5
mcaples <milescaples@gmail.com>
2014-05-08 00:09:33 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Clubhouse::Application.config.secret_key_base = '1a05dcbd4ea87e5c4020121190b324e86c737d584a006839c7e5e414714d1e4944ba9e265b555250bf2d895bbe3e7cb79e2eb1002e6b1a360420756547349501'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Clubhouse::Application.config.session_store :cookie_store, key: '_clubhouse_session'
